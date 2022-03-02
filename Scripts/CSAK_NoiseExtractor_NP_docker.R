729b735381f08a44595c37d103548fe789e1c8b0
garcia-nacho <garcia.nacho@gmail.com>
2022-03-02 13:08:52 +0100

    NoisePlotsForNanopore

--- Scripts/CSAK_NoiseExtractor_NP_docker.R
+++ Scripts/CSAK_NoiseExtractor_NP_docker.R
@@ -11,24 +11,21 @@ library(ggpubr)
 library(writexl)
 library(seqinr)
 
-# args=commandArgs(TRUE)
-#  cores<-10
-# if(length(args)==0){ 
-#   cores<-10
-# }else{
-#   cores<-as.numeric(gsub("c","",args[1]))
-#   }
+args=commandArgs(TRUE)
+ cores<-10
+if(length(args)==0){
+  cores<-10
+}else{
+  cores<-as.numeric(gsub("c","",args[1]))
+  }
 
-cores<-10
-# results <-"/home/docker/Fastq/"   #Docker
-# results<-list.dirs(results)
-# results<-results[grep("summaries$",results)]
-# results<-paste(results,"/",sep = "")
-# 
-# common.folder <-"/home/docker/CommonFiles/"
-# 
-# 
-results<-"/media/nacho/Data/contamintant_detection/Nanopore/"
+results <-"/home/docker/Fastq/"   #Docker
+results<-list.dirs(results)
+results<-results[grep("summaries$",results)]
+results<-paste(results,"/",sep = "")
+
+common.folder <-"/home/docker/CommonFiles/"
+
 temp <-paste(results,"rawnoise/",sep = "")   #Docker
 
 bamfiles<-list.files(results, pattern = ".bam$", full.names = TRUE, recursive = TRUE)
@@ -64,37 +61,20 @@ if(length(bamfiles)>0){
   
 
 # Plotting ----------------------------------------------------------------
-
+  p.noise<-function(x){
+    return(length(which(df$Noise[which(df$Base %in% stable.pos)]> x))/length(stable.pos))
+  }
+  p.minor<-function(x){
+    return(length(which(df$FreqMinor[which(df$Base %in% stable.pos)]> x))/length(stable.pos))
+  }
+  
   
   results.files<-list.files(temp, pattern = "_NoisExtractorResult\\.tsv$", full.names = TRUE)
 
-  samp<-c(1:length(results.files))
   
-  pb <- progress_bar$new(
-    format = "Index: :samp.pb [:bar] :elapsed | eta: :eta",
-    total = length(results.files),    # 100 
-    width = 60)
-  
-  
-  progress <- function(n){
-    pb$tick(tokens = list(samp.pb = samp[n]))
-  } 
-  
-  opts <- list(progress = progress)
-  
-  cluster.cores<-makeCluster(cores)
-  registerDoSNOW(cluster.cores)
-  
-  stable.pos<-read.csv("/media/nacho/Data/contamintant_detection/stable_positions_feb2022.csv")
-  stable.pos<-stable.pos$x
-  
-  out.par<-foreach(i=1:length(results.files), .verbose=FALSE, .options.snow = opts, .packages = c("ggplot2")) %dopar%{
-    p.noise<-function(x){
-      return(length(which(df$Noise[which(df$Base %in% stable.pos)]> x))/length(stable.pos))
-    }
-    
+  out.plots<-list()
+  for(i in 1:length(results.files)){
 
-    
     df<-read.csv(results.files[i],sep = "\t",header = FALSE)
     
     df<-df[,c(1,2,3,6,7)]
@@ -105,34 +85,40 @@ if(length(bamfiles)>0){
     
     stable.pos<-stable.pos[which(stable.pos %in% df$Base)]
     
-    df$p.value<-unlist(lapply(df$Noise, p.noise)) 
-    df$p.adj<-p.adjust(df$p.value, method = "fdr")  
     
     genome.position<-as.data.frame(c(1:29903))
     colnames(genome.position)<-"Base"
     df<-merge(genome.position, df, by="Base", all=TRUE)
     df$Outlier<-"NO"
-    df$Outlier[which(df$NoiseNP>0.95)]<-"YES"
+    
+    lowerq = quantile(df$FreqMinor,na.rm=TRUE)[2]
+    upperq = quantile(df$FreqMinor,na.rm=TRUE)[4]
+    iqr = upperq - lowerq 
+    co<-(iqr * 20) + upperq
+    
+    df$Outlier[which(df$FreqMinor>co)]<-"YES"
     
     df$Reads[which(is.na(df$Reads))]<-0
-    df$NoiseNP<-1-df$p.adj
-    df$NoiseNP[which(is.na(df$NoiseNP))]<-0
+
+    df$NoiseNP<-0
+    df$NoiseNP[which(df$Outlier=="YES")]<-df$FreqMinor[which(df$Outlier=="YES")]
+    
+    
     names<-gsub("\\.sorted.*","",gsub("_S[0-9].*","",gsub("R[0-9].*","",gsub(".*/","",results.files[i]))))
-    p<-ggplot(df)+
+    #names<-gsub("\\.primertrimmed.*","",gsub("_S[0-9].*","",gsub(".*/","",results.files[i])))
+    out.plots[[i]]<-ggplot(df)+
       geom_line(aes(Base, NoiseNP))+
       geom_point(data=subset(df, Outlier=="YES"),aes(Base, NoiseNP),col="red", alpha=0.3)+
       geom_point(data=subset(df, Reads<20),aes(Base, 0),col="blue", alpha=0.1)+
-      ylim(0,1)+
+      #ylim(0,1)+
       theme_minimal()+
       ggtitle(names)
-    return(list(p,df))
+
   }
   
- stopCluster(cluster.cores)
+
   
-  out.plots<-out.par
   
- 
 date<-gsub("-","",Sys.Date())
   
   if(length(out.plots)<=40 ){
