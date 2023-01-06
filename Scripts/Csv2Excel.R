a7b94c6a3a9026a5c81e8d072458826b480a0733
Nacho Garcia <garcia.nacho@gmail.com>
2023-01-06 15:08:09 +0100

    Csv to excel converter

---
+++ Scripts/Csv2Excel.R
@@ -0,0 +1,32 @@
+library(writexl)
+
+files<-list.files()
+
+if(length(grep(".csv$",files))==1){
+
+  if(length(grep("xlsx$", files))>0){
+    file.rename(files[grep("xlsx$",files)], gsub("xlsx$","xlsx_bk",files[grep("xlsx$",files)])) 
+  }
+  
+  file.toget<-files[grep(".csv$",files)]
+  if(length(file.toget)==1){
+    
+    opname<-gsub(".*Exp_","",file.toget)
+    opname<-gsub("_[0-9]+.csv","",opname)
+    
+    df<-read.csv(file.toget, stringsAsFactors = TRUE)
+    df$dummy<-NA
+    df<-df[,c(1,2,3,5,4)]
+    df<-apply(df,2,as.character)
+    
+    colnames(df)<-c("Posisjon  på PCR-plate",	"SequenceID",	"Barcode",	"Stamme-LabWare No.",	"Kons. (Ct.)")
+    dummy<-as.data.frame(t(colnames(df)))
+    colnames(dummy)<-colnames(df)
+    df<-rbind(dummy, df)
+  
+    colnames(df)<-rep(" ",ncol(df))
+    colnames(df)[2]<-gsub("-","",Sys.Date())
+    write_xlsx(df, paste(opname,".xlsx",sep = ""))
+
+  }
+}
