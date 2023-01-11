133fedcfbbe7d2b3d306a94ec2faccab4f0264b9
Nacho Garcia <garcia.nacho@gmail.com>
2023-01-11 13:19:58 +0100

    Update Csv2Excel.R

--- Scripts/Csv2Excel.R
+++ Scripts/Csv2Excel.R
@@ -14,13 +14,13 @@ if(length(grep(".csv$",files))==1){
     opname<-gsub(".*Exp_","",file.toget)
     opname<-gsub("_[0-9]+.csv","",opname)
     
-    df<-read.csv(file.toget, stringsAsFactors = FALSE)
+    df<-read.csv(file.toget, stringsAsFactors = TRUE)
     df$dummy<-NA
     df<-df[,c(1,2,3,5,4)]
     df<-apply(df,2,as.character)
     
     colnames(df)<-c("Posisjon  på PCR-plate",	"SequenceID",	"Barcode",	"Stamme-LabWare No.",	"Kons. (Ct.)")
-    df$Barcode<-gsub("BC","barcode",df$Barcode)
+    #df$Barcode<-gsub("BC","barcode",df$Barcode)
     dummy<-as.data.frame(t(colnames(df)))
     colnames(dummy)<-colnames(df)
     df<-rbind(dummy, df)
