fd69ee2ccc5c0f58f40416c970007a13f2b70b9f
RasmusKoRiis <79254436+RasmusKoRiis@users.noreply.github.com>
2024-08-20 15:20:52 +0200

    Bug fix nextclade_ouput_converter

--- Binaries/nextclade_output_converter.py
+++ Binaries/nextclade_output_converter.py
@@ -4,40 +4,41 @@
 
 import sys, csv, re
 
-def handleoutput(aasubs,aadels):
-	'''Takes a rows values for aa mutations and aa deletions and returns in dic form'''
-	outputdic = {k:[] for k in ["ORF1a", "ORF1b","S","S2","S3","ORF3a","E","M","ORF6","ORF7a","ORF7b","ORF8","N","ORF9b","ORF14","ORF10"]}
-	subs = aasubs.split(",")
-	dels = aadels.split(",") if not aadels.split(",") == [''] else []
-	#if dels == ['']:
-	#	dels = []
-	for elem in subs + dels:
-		if elem == '':
-			continue
-		prot,pos = elem.split(":")[0],elem.split(":")[1]
-		outputdic[prot] += [pos]
-	return outputdic
+def handleoutput(aasubs, aadels):
+    '''Takes a row's values for aa mutations and aa deletions and returns in dic form'''
+    outputdic = {k: [] for k in ["ORF1a", "ORF1b", "S", "S2", "S3", "ORF3a", "E", "M", "ORF6", "ORF7a", "ORF7b", "ORF8", "N", "ORF9b", "ORF14", "ORF10"]}
+    subs = aasubs.split(",")
+    dels = aadels.split(",") if not aadels.split(",") == [''] else []
+    # if dels == ['']:
+    #     dels = []
+    for elem in subs + dels:
+        if elem == '':
+            continue
+        prot, pos = elem.split(":")[0], elem.split(":")[1]
+        outputdic[prot] += [pos]
+    return outputdic
 
 with open(sys.argv[1]) as infile:
-	clades = csv.reader(infile,delimiter=";")
-	header = next(clades)
-	namecol = 1
-	try:
-		aasubcol = header.index("aaSubstitutions")
-		aadelcol = header.index("aaDeletions")
-	except ValueError:
-		aasubcol = 17
-		aadelcol = 19
+    clades = csv.reader(infile, delimiter=";")
+    header = next(clades)
+    namecol = 1
+    try:
+        aasubcol = header.index("aaSubstitutions")
+        aadelcol = header.index("aaDeletions")
+    except ValueError:
+        aasubcol = 17
+        aadelcol = 19
 
-	output = ["name","ORF1a", "ORF1b","S","S2","S3", "ORF3a","E","M","ORF6","ORF7a","ORF7b","ORF8","N","ORF9b","ORF14","ORF10"]
-	outstring = "\t".join(output)
-	print(outstring)
-	#outputdic = {{} for k in ["ORF1a", "ORF1b","S","S2", "ORF3a","E","M","ORF6","ORF7a","ORF7b","ORF8","N","ORF9b","ORF14","ORF10"]}
-	for row in clades:
-		name = row[namecol]
-		muts = row[aasubcol]
-		dels = row[aadelcol]
-		allchanges = handleoutput(muts,dels)
+    output = ["name", "ORF1a", "ORF1b", "S", "S2", "S3", "ORF3a", "E", "M", "ORF6", "ORF7a", "ORF7b", "ORF8", "N", "ORF9b", "ORF14", "ORF10"]
+    outstring = "\t".join(output)
+    print(outstring)
+
+    for row in clades:
+        name = row[namecol]
+        muts = row[aasubcol]
+        dels = row[aadelcol]
+        allchanges = handleoutput(muts, dels)
+
         if len(allchanges['S']) <= 22:
             allchanges['S2'] = []
             allchanges['S3'] = []
@@ -49,9 +50,10 @@ with open(sys.argv[1]) as infile:
             allchanges['S3'] = allchanges['S'][44:]
             allchanges['S2'] = allchanges['S'][22:44]
             allchanges['S'] = allchanges['S'][:22]
-		outstring = ''
-		for prot in ["ORF1a", "ORF1b","S","S2","S3","ORF3a","E","M","ORF6","ORF7a","ORF7b","ORF8","N","ORF9b","ORF14","ORF10"]:
-			outstring += ";".join(allchanges[prot])
-			outstring += "\t"
-		outstring.rstrip()
-		print(name + "\t" + outstring)
+
+        outstring = ''
+        for prot in ["ORF1a", "ORF1b", "S", "S2", "S3", "ORF3a", "E", "M", "ORF6", "ORF7a", "ORF7b", "ORF8", "N", "ORF9b", "ORF14", "ORF10"]:
+            outstring += ";".join(allchanges[prot])
+            outstring += "\t"
+        outstring = outstring.rstrip()
+        print(name + "\t" + outstring)
