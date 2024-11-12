30af88cf102befe60adfd43559ddcc63dcdc44de
RasmusKoRiis <79254436+RasmusKoRiis@users.noreply.github.com>
2024-11-12 11:56:10 +0100

    Fixed bug

--- Binaries/nextclade_output_converter.py
+++ Binaries/nextclade_output_converter.py
@@ -2,14 +2,15 @@
 
 '''Script for converting a Nextclade output to Karoline Bragstad-format'''
 
-import sys, csv, re
+import sys, csv
 
+# Custom function for handling output
 def handleoutput(aasubs, aadels):
-    '''Takes a row's values for aa mutations and aa deletions and returns in dic form'''
-    outputdic = {k: [] for k in ["ORF1a", "ORF1b", "S", "S2", "S3", "S4", "S5", "ORF3a", "E", "M", "ORF6", "ORF7a", "ORF7b", "ORF8", "N", "ORF9b", "ORF14", "ORF10", "ORF1a_1"]}
+    '''Takes a row's values for aa mutations and aa deletions and returns in dict form'''
+    outputdic = {k: [] for k in ["ORF1a", "ORF1b", "S", "S2", "S3", "S4", "S5", "ORF3a", "E", "M", "ORF6", "ORF7a", "ORF7b", "ORF8", "N", "ORF9b", "ORF14", "ORF10"]}
     subs = aasubs.split(",")
     dels = aadels.split(",") if not aadels.split(",") == [''] else []
-
+    
     for elem in subs + dels:
         if elem == '':
             continue
@@ -17,64 +18,43 @@ def handleoutput(aasubs, aadels):
         outputdic[prot] += [pos]
     return outputdic
 
-with open(sys.argv[1]) as infile:
-    clades = csv.reader(infile, delimiter=";")
-    header = next(clades)
-    namecol = 1
-    try:
-        aasubcol = header.index("aaSubstitutions")
-        aadelcol = header.index("aaDeletions")
-    except ValueError:
-        aasubcol = 17
-        aadelcol = 19
+# Output headers
+output_headers = ["name", "ORF1a", "ORF1b", "S", "S2", "S3", "S4", "S5", "ORF3a", "E", "M", "ORF6", "ORF7a", "ORF7b", "ORF8", "N", "ORF9b", "ORF14", "ORF10"]
 
-    output_headers = ["name", "ORF1a", "ORF1a_1", "ORF1b", "S", "S2", "S3", "S4", "S5", "ORF3a", "E", "M", "ORF6", "ORF7a", "ORF7b", "ORF8", "N", "ORF9b", "ORF14", "ORF10"]
-    print("\t".join(output_headers))
+# Processing each row in the CSV file
+final_results = []
+input_file_path = sys.argv[1]
+output_file_path = sys.argv[2]
+
+with open(input_file_path) as infile:
+    clades = csv.reader(infile, delimiter=";")
+    header = next(clades)  # Read header
 
     for row in clades:
-        name = row[namecol]
-        muts = row[aasubcol]
-        dels = row[aadelcol]
+        name = row[1]  # Using 'seqName' as the 'name' column
+        muts = row[29]  # 'aaSubstitutions' column
+        dels = row[30]  # 'aaDeletions' column
         allchanges = handleoutput(muts, dels)
 
-        if len(allchanges['S']) <= 22:
-            allchanges['S2'] = []
-            allchanges['S3'] = []
-            allchanges['S4'] = []
-            allchanges['S5'] = []
-        elif len(allchanges['S']) <= 44:
-            allchanges['S2'] = allchanges['S'][22:]
-            allchanges['S'] = allchanges['S'][:22]
-            allchanges['S3'] = []
-            allchanges['S4'] = []
-            allchanges['S5'] = []
-        elif len(allchanges['S']) <= 66:
-            allchanges['S2'] = allchanges['S'][22:44]
-            allchanges['S'] = allchanges['S'][:22]
-            allchanges['S3'] = allchanges['S'][44:]
-            allchanges['S4'] = []
-            allchanges['S5'] = []
-        elif len(allchanges['S']) <= 88:
-            allchanges['S2'] = allchanges['S'][22:44]
-            allchanges['S'] = allchanges['S'][:22]
-            allchanges['S3'] = allchanges['S'][44:66]
-            allchanges['S4'] = allchanges['S'][66:]
-            allchanges['S5'] = []
-        else:
-            allchanges['S2'] = allchanges['S'][22:44]
-            allchanges['S'] = allchanges['S'][:22]
-            allchanges['S3'] = allchanges['S'][44:66]
-            allchanges['S4'] = allchanges['S'][66:88]
-            allchanges['S5'] = allchanges['S'][88:]
+        # Applying corrected segmentation logic for S parts
+        initial_S = allchanges['S']
+        allchanges['S'] = initial_S[:22]
+        allchanges['S2'] = initial_S[22:44]
+        allchanges['S3'] = initial_S[44:66]
+        allchanges['S4'] = initial_S[66:88]
+        allchanges['S5'] = initial_S[88:]
 
-        # Splitting 'ORF1a' into 'ORF1a' and 'ORF1a_1'
-        if len(allchanges['ORF1a']) <= 22:
-            allchanges['ORF1a_1'] = []
-        else:
-            allchanges['ORF1a_1'] = allchanges['ORF1a'][22:]
-            allchanges['ORF1a'] = allchanges['ORF1a'][:22]
-
+        # Constructing the output for each row
         outstring = name
         for prot in output_headers[1:]:  # Skip "name"
             outstring += "\t" + ";".join(allchanges[prot])
-        print(outstring)
+        final_results.append(outstring)
+
+# Write the header and final results to the output file
+with open(output_file_path, 'w') as outfile:
+    # Writing header
+    outfile.write("\t".join(output_headers) + "\n")
+    
+    # Writing each processed row to the file
+    for result in final_results:
+        outfile.write(result + "\n")
